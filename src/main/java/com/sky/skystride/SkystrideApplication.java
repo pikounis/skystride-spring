6f26da7d05bbe920c7535fee72f3f908a9a89ef0
Anastasios Pikounis <anastasiospikounis@gmail.com>
2024-09-26 09:44:09 +0100

    first commit

---
+++ src/main/java/com/sky/skystride/SkystrideApplication.java
@@ -0,0 +1,13 @@
+package com.sky.skystride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkystrideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkystrideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/skystride/SkystrideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.skystride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkystrideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
