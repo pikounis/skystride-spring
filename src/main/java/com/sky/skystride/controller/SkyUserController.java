e373ede815eb10d7a5b0270dc6c523afc5559882
Pallav <pallavhingu@gmail.com>
2024-09-26 12:06:49 +0100

    added release properties

--- src/main/java/com/sky/skystride/controller/SkyUserController.java
+++ src/main/java/com/sky/skystride/controller/SkyUserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class SkyUserController {
 
     private SkyUserService service;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into sky_user(first_name, last_name, current_timer_running, timer_start_time, started_sport)
+values ('Julie', 'Dooley', False, NULL, NULL),
+('Bob', 'Smith', False, NULL, NULL);
