a38dccf6cc58e632de20aee571c76c5a606de777
Pallav <pallavhingu@gmail.com>
2024-09-26 17:14:16 +0100

    changed CORS port to 80

--- src/main/java/com/sky/skystride/controller/SkyUserController.java
+++ src/main/java/com/sky/skystride/controller/SkyUserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:80")
 public class SkyUserController {
 
     private SkyUserService service;
