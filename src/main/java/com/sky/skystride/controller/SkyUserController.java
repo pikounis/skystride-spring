408697eca0273b7ba4211adf40aeae464cf96f82
Pallav <pallavhingu@gmail.com>
2024-09-26 17:17:55 +0100

    changed cors policy url again

--- src/main/java/com/sky/skystride/controller/SkyUserController.java
+++ src/main/java/com/sky/skystride/controller/SkyUserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:80")
+@CrossOrigin(origins = "http://35.176.106.162")
 public class SkyUserController {
 
     private SkyUserService service;
