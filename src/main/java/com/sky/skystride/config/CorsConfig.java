70540eafd50a405a12d6092943409707e6a4979e
Nicuu14 <nicolae.savin14@gmail.com>
2024-10-02 10:26:16 +0100

    commented Spring Security

--- src/main/java/com/sky/skystride/config/CorsConfig.java
+++ src/main/java/com/sky/skystride/config/CorsConfig.java
@@ -1,18 +1,18 @@
-package com.sky.skystride.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Allow your frontend origin
-                .allowedMethods("*")                     // Allow all HTTP methods
-                .allowedHeaders("*")                     // Allow all headers
-                .allowCredentials(true);                 // Allow credentials (e.g., cookies)
-    }
-}
+//package com.sky.skystride.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000") // Allow your frontend origin
+//                .allowedMethods("*")                     // Allow all HTTP methods
+//                .allowedHeaders("*")                     // Allow all headers
+//                .allowCredentials(true);                 // Allow credentials (e.g., cookies)
+//    }
+//}
