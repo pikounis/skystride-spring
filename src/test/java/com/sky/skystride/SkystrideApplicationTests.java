9667f2e2d47f37c208413aafd7c440510850afe6
Pallav <pallavhingu@gmail.com>
2024-10-04 13:38:43 +0100

    remove test and change remote url

--- src/test/java/com/sky/skystride/SkystrideApplicationTests.java
+++ src/test/java/com/sky/skystride/SkystrideApplicationTests.java
@@ -3,11 +3,11 @@ package com.sky.skystride;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SkystrideApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class SkystrideApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
